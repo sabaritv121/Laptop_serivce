606c61dd284acd38bf4236bdcb3054fab2220d1c
sabaritv121 <sabaritv121@gmail.com>
2024-04-29 10:39:24 +0530

    'stock'

--- service_app/customer_views.py
+++ service_app/customer_views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
+from service_app import models
 from service_app.filters import PlaceFilter, AppointmentFilter
 from service_app.forms import FeedbackForm
 from service_app.models import AppointmentSchedule, Customer, Appointment, Sales_add, Cart, Complaints
@@ -56,37 +57,62 @@ def cus_view_items(request):
 def Add_to_cart(request, id):
     sale = Sales_add.objects.get(id=id)
     u = Customer.objects.get(user=request.user)
-    # booking = Cart.objects.filter(user=u ,sale=sale)
-    # if booking.exists():
-    #     messages.info(request, 'You Have Already Booked this product')
-    #     return redirect("cus_view_items")
-    # else:
+
     if request.method == 'POST':
-            obj = Cart()
-            obj.user = u
-            obj.sale = sale
-            qty = request.POST.get("quantity")
-            address = request.POST.get("address")
-            mobile = request.POST.get("mobile")
-            obj.quantity = qty
-            obj.address = address
-            obj.mobile = mobile
-            obj.save()
-            try:
+        qty = int(request.POST.get("quantity", 0))
+        address = request.POST.get("address")
+        mobile = request.POST.get("mobile")
 
-                qty = int(qty)
-            except ValueError:
+        # Check if the requested quantity is available
+        if qty > sale.quantity:
+            return render(request, 'customer/no_stock.html')  # Redirect to no stock available page
 
-                qty = 0
+        obj = Cart(user=u, sale=sale, quantity=qty, address=address, mobile=mobile)
+        obj.save()
 
-            # Now perform the subtraction operation
+        # Update sale quantity
+        sale.quantity -= qty
+        sale.save()
 
-            sale.quantity = sale.quantity - qty
-            print(sale.quantity)
-            sale.save()
-            messages.info(request, 'Item added to My orders')
-            return redirect('cus_view_items')
+        messages.info(request, 'Item added to My orders')
+        return redirect('cus_view_items')
+
     return render(request, 'customer/bookings.html', {'schedule': sale})
+#
+# def Add_to_cart(request, id):
+#     sale = Sales_add.objects.get(id=id)
+#     u = Customer.objects.get(user=request.user)
+#     # booking = Cart.objects.filter(user=u ,sale=sale)
+#     # if booking.exists():
+#     #     messages.info(request, 'You Have Already Booked this product')
+#     #     return redirect("cus_view_items")
+#     # else:
+#     if request.method == 'POST':
+#             obj = Cart()
+#             obj.user = u
+#             obj.sale = sale
+#             qty = request.POST.get("quantity")
+#             address = request.POST.get("address")
+#             mobile = request.POST.get("mobile")
+#             obj.quantity = qty
+#             obj.address = address
+#             obj.mobile = mobile
+#             obj.save()
+#             try:
+#
+#                 qty = int(qty)
+#             except ValueError:
+#
+#                 qty = 0
+#
+#             # Now perform the subtraction operation
+#
+#             sale.quantity = sale.quantity - qty
+#             print(sale.quantity)
+#             sale.save()
+#             messages.info(request, 'Item added to My orders')
+#             return redirect('cus_view_items')
+#     return render(request, 'customer/bookings.html', {'schedule': sale})
 
 
 def My_list(request):
